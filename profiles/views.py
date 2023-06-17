016772cfa76fd4e29db479199ee43f35eb91f4e1
CBergane <christian.bergane@gmail.com>
2023-06-17 21:54:32 +0000

    fixed some minor ishues with friendslist and links in it, added elete function from the list, the list will show for all users but only owner can delete and accept

--- profiles/views.py
+++ profiles/views.py
@@ -84,14 +84,25 @@ class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = "profile.html"
 
     def get_context_data(self, **kwargs):
-        profile = None
+        profile = Profile.objects.get(user=self.kwargs["pk"])
         friend_request_received = None
+        friends = None
         if self.request.user.is_authenticated:
-            profile = Profile.objects.get(user=self.kwargs["pk"])
             friend_request_received = profile.received_friend_requests.all()
+            if self.request.user == profile.user:
+                # If the logged-in user is viewing their own profile,
+                # retrieve all friends
+                friends = profile.friends.all()
+            else:
+                # If the logged-in user is viewing another profile,
+                # check if they are friends with the profile owner
+                logged_in_user_profile = Profile.objects.get(user=self.request.user)
+                if logged_in_user_profile.friends.filter(user=profile.user).exists():
+                    friends = profile.friends.all()
         context = {
             'profile': profile,
             'friend_request_received': friend_request_received,
+            'friends': friends,
             'form': ProfileForm(instance=profile)
         }
         return context
@@ -100,10 +111,8 @@ class ProfileView(LoginRequiredMixin, TemplateView):
         profile = Profile.objects.get(user=self.request.user)
         friend_username = request.POST.get('friend_username')
         friend_profile = Profile.objects.get(user__username=friend_username)
-
         # send request
         profile.send_friend_request(friend_profile.user)
-
         return redirect('profile', pk=profile.pk)
 
 
