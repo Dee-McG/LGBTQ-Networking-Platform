df01d16c0fc266a7606b94b185f34af7936d1634
Daisy McGirr <mcgirr.daisy@gmail.com>
2023-06-18 20:16:02 +0100

    Add group functionality and templates

--- groups/forms.py
+++ groups/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from djrichtextfield.widgets import RichTextWidget
-from .models import Group
+from .models import Group, GroupPost, GroupComments
 
-class GroupForm (forms.ModelForm):
+class GroupForm(forms.ModelForm):
     class Meta:
         model = Group
         fields = ['group_name', 'image', 'bio']
@@ -11,4 +11,25 @@ class GroupForm (forms.ModelForm):
             'group_name': 'Group Name',
             'image': 'Group Image',
             'bio': 'Group Info',
+        }
+
+
+class GroupPostForm(forms.ModelForm):
+    class Meta:
+        model = GroupPost
+        fields = ['post', 'image']
+
+        labels = {
+            'image': 'Post Image',
+            'post': 'Post',
+        }
+
+
+class GroupCommentForm(forms.ModelForm):
+    class Meta:
+        model = GroupComments
+        fields = ['comment']
+
+        labels = {
+            'comment': 'Comment'
         }
